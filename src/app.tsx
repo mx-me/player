54becd613e0d2ed961e65d0c1ba2c36bbdd19c6c
mx-me <mximef@proton.me>
2025-12-04 23:58:37 +0100

    add audio context and pass it to app context

--- src/app.tsx
+++ src/app.tsx
@@ -1,10 +1,45 @@
+import {
+  createContext,
+  Ref,
+  RefObject,
+  useContext,
+  useEffect,
+  useRef,
+} from 'react'
+import { Tracklist } from './components/tracklist'
 import './index.css'
 
+interface AppContext {
+  currentTrack: string
+  audio: RefObject<AudioContext | null>
+}
+
+export const AppContext = createContext<AppContext | null>(null)
+
 export function App() {
+  const audioRef = useRef<AudioContext>(null)
+
+  useEffect(() => {
+    audioRef.current = new AudioContext()
+
+    return () => {
+      if (audioRef.current && audioRef.current.state !== 'closed') {
+        audioRef.current.close()
+      }
+    }
+  }, [])
+
   return (
-    <main>
-      hello
-    </main>
+    <AppContext
+      value={{
+        currentTrack: '',
+        audio: audioRef,
+      }}
+    >
+      <main>
+        <Tracklist />
+      </main>
+    </AppContext>
   )
 }
 

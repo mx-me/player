98c345f84de27901a7fd2d17053a507b51d2b4a4
mx-me <mximef@proton.me>
2025-12-04 21:33:33 +0100

    init

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "Bun flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      supportedSystems = nixpkgs.lib.systems.flakeExposed;
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+    in
+    {
+      devShells = forAllSystems (
+        system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+          };
+        in
+        {
+          default = pkgs.mkShell {
+            packages = with pkgs; [
+              bun
+            ];
+          };
+        }
+      );
+    };
+}
---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './index.css'
+
+export function App() {
+  return (
+    <main>
+      hello
+    </main>
+  )
+}
+
+export default App

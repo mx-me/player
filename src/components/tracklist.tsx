7e400c3134ef4d88d907916e4a7ae0c23018c08a
mx-me <mximef@proton.me>
2025-12-05 01:29:07 +0100

    load track list

--- src/components/tracklist.tsx
+++ src/components/tracklist.tsx
@@ -1,9 +1,48 @@
 import { AppContext } from '@/app'
-import { useContext } from 'react'
+import { useContext, useEffect, useState } from 'react'
+
+interface Track {
+  id: number
+  title: string
+  location: string
+  cover: string
+  blurhash: string
+}
 
 export const Tracklist = () => {
-  const context = useContext(AppContext)
-  if (!context?.audio.current) throw 'could not get audio context'
+  const { audioManager } = useContext(AppContext)
+  const [tracks, setTracks] = useState<Track[]>()
 
-  return <section>hi</section>
+  useEffect(() => {
+    if (!audioManager) return
+
+    const { abort, signal } = new AbortController()
+
+    fetch('/tracks.json', { signal }).then((req) =>
+      req.json().then((tracks) => {
+        console.log(tracks)
+        setTracks(tracks)
+      }),
+    )
+
+    return () => {
+      abort()
+    }
+  }, [audioManager])
+
+  if (!tracks || !audioManager) {
+    return <section>loading tracks...</section>
+  }
+
+  const changeTrack = (id: string) => {}
+
+  return (
+    <section>
+      <ul>
+        {tracks.map(({ blurhash, cover, id, location, title }) => (
+          <li key={id}>{title}</li>
+        ))}
+      </ul>
+    </section>
+  )
 }

7d711a898734b322003d361c7023df2dcf934335
mx-me <mximef@proton.me>
2025-12-05 01:47:07 +0100

    implement play pause setvolume and changetrack methods

--- src/audio.ts
+++ src/audio.ts
@@ -1,12 +1,12 @@
 export class Audio {
   private context
-  // private element
+  private element
   private sourceNode
   private gainNode
 
   constructor(mediaElement: HTMLAudioElement) {
     this.context = new AudioContext()
-    // this.element = mediaElement
+    this.element = mediaElement
 
     this.sourceNode = this.context.createMediaElementSource(mediaElement)
     this.gainNode = this.context.createGain()
@@ -15,6 +15,28 @@ export class Audio {
     this.gainNode.connect(this.context.destination)
 
     this.gainNode.gain.value = 0.5
+  }
+
+  public setVolume(volume: number) {
+    this.gainNode.gain.value = volume
+  }
+
+  public play() {
+    if (this.context.state === 'suspended') {
+      this.context.resume()
+    }
+    this.element.play()
+  }
+
+  public pause() {
+    this.element.pause()
+    this.context.suspend()
+  }
+
+  public changeTrack(url: string) {
+    this.pause()
+    this.element.src = url
+    this.play()
   }
 
   public destroy() {

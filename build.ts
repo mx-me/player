fa648fa0a3137652b0751cd6b0148d94678a41e7
mx-me <mximef@proton.me>
2025-12-05 04:43:23 +0100

    add build script

---
+++ build.ts
@@ -0,0 +1,23 @@
+import { Glob, file, write, build } from 'bun'
+import { mkdir } from 'fs'
+
+const outDir = './dist'
+const staticDir = './static'
+
+await build({
+  entrypoints: ['./src/index.html'],
+  outdir: outDir,
+  minify: true,
+  define: { 'process.env.NODE_ENV': 'production' },
+  env: 'BUN_PUBLIC_*',
+  target: 'browser'
+})
+
+const staticFiles = new Glob(`${staticDir}/**/*`)
+
+for await (const path of staticFiles.scan({ onlyFiles: true })) {
+  const destPath = path.replace(staticDir, outDir)
+  const destDir = destPath.substring(0, destPath.lastIndexOf('/'))
+  mkdir(destDir, { recursive: true }, () => { })
+  await write(destPath, file(path))
+}

5cbd984f4352e03309838e19af1a4796c1790a26
mx-me <mximef@proton.me>
2025-12-08 01:50:15 +0100

    rename audio manager class and simplify it, create the audio element within the class itself do not render it to the dom

--- src/manager.ts
+++ src/manager.ts
@@ -1,26 +1,28 @@
 import { getBlurHashAverageColor } from 'fast-blurhash'
 import { Track } from './components/tracklist'
 
-export class Audio {
+export class Manager {
   private context
   private sourceNode
   private gainNode
 
-  public element
-
   public color?: [number, number, number]
 
-  constructor(mediaElement: HTMLAudioElement) {
-    this.context = new AudioContext()
-    this.element = mediaElement
+  constructor() {
+    const audio = new Audio()
 
-    this.sourceNode = this.context.createMediaElementSource(mediaElement)
+    this.context = new AudioContext()
+    this.sourceNode = this.context.createMediaElementSource(audio)
     this.gainNode = this.context.createGain()
 
     this.sourceNode.connect(this.gainNode)
     this.gainNode.connect(this.context.destination)
 
     this.gainNode.gain.value = 0.5
+  }
+
+  public get audio() {
+    return this.sourceNode.mediaElement
   }
 
   public setVolume(volume: number) {
@@ -31,17 +33,17 @@ export class Audio {
     if (this.context.state === 'suspended') {
       this.context.resume()
     }
-    this.element.play()
+    this.audio.play()
   }
 
   public pause() {
-    this.element.pause()
+    this.audio.pause()
     this.context.suspend()
   }
 
   public changeTrack({ location, cover, title, blurhash }: Track) {
     this.pause()
-    this.element.src = location
+    this.audio.src = location
     this.play()
 
     if (blurhash) {
@@ -57,10 +59,14 @@ export class Audio {
   }
 
   public destroy() {
+    this.audio.pause()
+
     if (this.context.state !== 'closed') {
       this.sourceNode.disconnect()
       this.gainNode.disconnect()
       this.context.close()
     }
+
+    this.audio.remove()
   }
 }

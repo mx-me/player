4cce2e3337f6095c78e2c33e52d94d2621ffc7be
mx-me <mximef@proton.me>
2025-12-09 22:26:59 +0100

    remove debug log

--- src/components/seek.tsx
+++ src/components/seek.tsx
@@ -50,7 +50,6 @@ export const Seek = () => {
 
       const offsetX = clientX - rect.left
       let seekPercent = offsetX / rect.width
-      console.log(seekPercent, duration)
 
       if (!seekPercent) return
       if (seekPercent < 0) seekPercent = 0
